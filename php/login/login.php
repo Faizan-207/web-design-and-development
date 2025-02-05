06556f703d5bb4815899fc7cc60aa9271e80e7a2
Faizan ali <faizanakmal581@gmail.com>
2025-02-05 23:54:07 +0500

    login data

---
+++ php/login/login.php
@@ -0,0 +1,22 @@
+<!-- <?php
+require_once "connection.php";
+
+$username = "faizanali@gmail.com";
+$password = "207";
+
+$sql = "INSERT INTO login-form (`username`, `password`) VALUES ('$username', '$password')";
+$result = mysqli_query($conn,$sql);
+
+if ($result){
+    echo "hi";
+}
+
+?>
+
+ -->
+<?php
+
+
+    echo "Hello world";
+
+?>
