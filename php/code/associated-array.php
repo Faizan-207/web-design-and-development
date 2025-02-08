292080763185a1f4d5e99e5fee33af4e9c5efe42
Faizan ali <faizanakmal581@gmail.com>
2025-02-09 01:59:51 +0500

    associated array

---
+++ php/code/associated-array.php
@@ -0,0 +1,12 @@
+<?php
+
+$students = [1 => "faizan",2 => "abdullah",3 => "toqeer"];
+
+foreach ($students as $key => $value){
+    echo "$key" ."$value"."<br>";
+}
+
+
+
+
+?>
---
+++ php/code/loops.php
@@ -0,0 +1,12 @@
+<?php
+
+$value = 1;
+
+while ($value < 10){
+
+    echo $value;
+    echo "<br>";
+    $value++;
+}
+
+?>
