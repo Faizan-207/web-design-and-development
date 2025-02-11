edb853abad44a8fe06a3b5fc80dc60f5f6523f9b
Faizan ali <faizanakmal581@gmail.com>
2025-02-11 19:16:48 +0500

    multidimensional array

---
+++ php/code/multidimensional-array.php
@@ -0,0 +1,9 @@
+<?php
+$students = [["faizan","21","bscs"],["abdullah","20","bscs"],["toqeer","22","bscs"]];
+
+foreach($students as $student){
+    echo "Name: ".$student[0]." Age: ".$student[1]." Dprt: ".$student[2]."<br>";
+}
+
+
+?>
