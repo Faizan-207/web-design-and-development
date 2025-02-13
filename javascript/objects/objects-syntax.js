8f2b1468dbf1ef96a7b3deb006dd6c32e194dfcd
Faizan ali <faizanakmal581@gmail.com>
2025-02-14 04:04:50 +0500

    Objects in Js

---
+++ javascript/objects/objects-syntax.js
@@ -0,0 +1,9 @@
+const students = {
+    name:"Faizan ali",
+    marks: 94.5,
+    age:21,
+    dprt:"computer science",
+    printMarks: function (){
+        console.log("Marks :" ,this.marks);
+    }
+};
